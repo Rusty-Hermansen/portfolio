dab28caab13ac78aac78bbb5747d327ecbbdcf1b
Rusty Hermansen <dr.delight@gmail.com>
2021-11-10 22:32:44 -0700

    tweaked text size and icon size on nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,25 @@ import {SocialIcon} from "react-social-icons"
 const Navbar = () => {
 
     return(
-        <header className="bg-gray-900">
+        <header className="bg-gray-900 sticky top-0 z-50">
             <div className="container mx-auto flex justify-between ">
                 <nav className='flex'> 
                     <NavLink to='/' exact 
+                    style={{ textDecoration: 'none' }}
                     activeClassName='text-yellow-200' 
-                    className='inflex-flex items-center py-3 px-3 my-6 text-yellow-700 hover:text-white font-bold '>
+                    className='inflex-flex items-center py-2 px-3 my-6 text-yellow-700 hover:text-white font-bold text-2xl'>
                         Rusty Hermansen
                     </NavLink>
                     <NavLink to='/posts' 
+                    style={{ textDecoration: 'none' }}
                     activeClassName='text-yellow-200' 
-                    className='inflex-flex items-center py-3 px-3 my-6 text-yellow-700 hover:text-white font-bold '> 
+                    className='inflex-flex items-center py-2 px-3 my-6 text-yellow-700 hover:text-white font-bold text-2xl'> 
                         Portfolio Post
                     </NavLink>
                     <NavLink to='about' 
+                    style={{ textDecoration: 'none' }}
                     activeClassName='text-yellow-200'
-                    className='inflex-flex items-center py-3 px-3 my-6 text-yellow-700 hover:text-white  font-bold '>
+                    className='inflex-flex items-center py-2 px-3 my-6 text-yellow-700 hover:text-white  font-bold text-2xl'>
                         About
                     </NavLink>
                 </nav>
