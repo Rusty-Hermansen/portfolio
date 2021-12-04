1aaf373c140ba7d72c638974e804e1defaf80cca
Rusty Hermansen <dr.delight@gmail.com>
2021-12-03 20:27:05 -0700

    Added .gitlab-ci.yml file for CI/CD pipeline

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,51 @@
 import {useState, useEffect} from 'react';
 import { Link} from 'react-router-dom'
 import apiService from '../Services/apiService';
-import { posts } from '../posts/posts';
+// import { posts } from '../posts/posts';
 
+/////////////////////This piece handles RETRIEVAL FROM DATABASE////////////////////////
+// const Post = () =>{
 
+//     const [dbPosts, setDbPosts] = useState([]);
+
+//     useEffect(() => {
+//         apiService.getPosts().then(r => setDbPosts(r))
+//     }, [])
+   
+//     return(
+//         <main className="bg-black min-h-screen p-12"> 
+//             <section className = "container mx-auto">
+//                 <h1 className="text-5xl flex justify-center mb-12 text-white">Portfolio Posts</h1>
+                
+//                 <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
+             
+//                     { dbPosts && dbPosts.map((post)=> (                                                                                                   
+//                        <article>
+//                         <Link to={"/singlepost/" + post.id} key={post.id} state={{id: post.id}}>                                                            
+//                           <span className="block h-64 relative rouded shadow leading-snug bg-white border-l-8 border-black" key={post.id}>                  
+//                              <img src={post.post_img} alt={post.alt} className="w-full h-full object-contain absolute"/>                                    
+//                              <span className="block relative h-full flex justify-end items-end pr-4 pb-4">                                                  
+//                                 <h3 className="text-white text-lg font-blog px-3 py-4 bg-black text-white bg-opacity-75 rounded" >{post.title} </h3>        
+//                              </span>                                                                                                                        
+//                              </span>                                                                                                                        
+//                         </Link>
+//                     </article> 
+//                     ))
+//                     }
+//                 </div>
+//             </section>
+//         </main>
+//     )
+// }
+
+//////////////////////////////////This piece is to handle LOCAL POSTS///////////////////////////////////////
 const Post = () =>{
 
     const [dbPosts, setDbPosts] = useState([]);
 
-    // useEffect(() => {
-    //     apiService.getPosts().then(r => setDbPosts(r))
-    // }, [])
+    useEffect(() => {
+        apiService.getPosts().then(r => setDbPosts(r))
+    }, [])
    
     return(
         <main className="bg-black min-h-screen p-12"> 
@@ -18,24 +53,15 @@ const Post = () =>{
                 <h1 className="text-5xl flex justify-center mb-12 text-white">Portfolio Posts</h1>
                 
                 <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
-             
-                    {/* { dbPosts && dbPosts.map((post)=> ( */}
-                    { posts && posts.map((post, index)=> (
-                       <article>
-                        {/* <Link to={"/singlepost/" + post.id} key={post.id} state={{id: post.id}}>
-                          <span className="block h-64 relative rouded shadow leading-snug bg-white border-l-8 border-black" key={post.id}>
-                             <img src={post.post_img} alt={post.alt} className="w-full h-full object-contain absolute"/>
-                             <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                <h3 className="text-white text-lg font-blog px-3 py-4 bg-black text-white bg-opacity-75 rounded" >{post.title} </h3>
-                             </span> 
-                             </span>*/}
-                        <Link to={"/singlepost/" + index} key={index} state={{body: post.body}}>
-                        <span className="block h-64 relative rouded shadow leading-snug bg-white border-l-8 border-black" key={index}>
-                             <img src={post.img} alt={post.alt} className="w-full h-full object-contain absolute"/>
-                             <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                <h3 className="text-white text-lg font-blog px-3 py-4 bg-black text-white bg-opacity-75 rounded" >{post.title} </h3>
-                             </span>
-                        </span>
+                     { posts && posts.map((post, index)=> (                                                                                                            
+                    <article>
+                        <Link to={"/singlepost/" + index} key={index} state={{body: post.body}}>                                                                    
+                        <span className="block h-64 relative rouded shadow leading-snug bg-white border-l-8 border-black" key={index}>                                  
+                             <img src={post.img} alt={post.alt} className="w-full h-full object-contain absolute"/>                                                     
+                             <span className="block relative h-full flex justify-end items-end pr-4 pb-4">                                                              
+                                <h3 className="text-white text-lg font-blog px-3 py-4 bg-black text-white bg-opacity-75 rounded" >{post.title} </h3>                    
+                             </span>                                                                                                                                    
+                        </span>                                                                                                                                     
                         </Link>
                     </article> 
                     ))
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -8,34 +8,46 @@ const SinglePost = () => {
     const params = useParams();
 
     const post = posts[params.id]
-
-    // const [dbPost, setDbPost] = useState([]);
-
-    // useEffect(() => {
-    //     apiService.getPostById(params.id).then(r => setDbPost(r))
-    // }, [])
  
     return(
         // <div className="justify-center bg-black p-12">
         <section className="justify-center min-h-screen pt-12 lg:pt-36 px-8 bg-black bg-opacity-75">
-            {/* <h1 className="text-5xl flex justify-center mb-12 text-white">{dbPost[0].title}</h1>
-            {parse(dbpost)}
-            { dbPost && dbPost.map((post)=> {
-                if(dbPost.text !== null){
-                    return( dbPost.text)
-                   
-                }
-                else {
-                    return
-                    dbPost.image
-                }
-            })
-                    } */}
-         {/* </div> */}
          <h1 className="text-5xl flex justify-center mb-12 text-white">{post.title}</h1>
             {parse(post.body)}
+            {/* </div> */}
         </section>
     )
 }
+
+//////////////////////////////////////This is for DATABASE INTERACTION///////////////////////////////////////
+// const SinglePost = () => {
+
+//     const [dbPost, setDbPost] = useState([]);
+
+//     useEffect(() => {
+//         apiService.getPostById(params.id).then(r => setDbPost(r))
+//     }, [])
+ 
+//     return(
+//         // <div className="justify-center bg-black p-12">
+//         <section className="justify-center min-h-screen pt-12 lg:pt-36 px-8 bg-black bg-opacity-75">
+//             <h1 className="text-5xl flex justify-center mb-12 text-white">{dbPost[0].title}</h1>
+//             {parse(dbpost)}
+//             { dbPost && dbPost.map((post)=> {
+//                 if(dbPost.text !== null){
+//                     return( dbPost.text)
+                   
+//                 }
+//                 else {
+//                     return
+//                     dbPost.image
+//                 }
+//             })
+//                     }
+//          {/* </div> */}
+       
+//         </section>
+//     )
+// }
 
 export default SinglePost;
